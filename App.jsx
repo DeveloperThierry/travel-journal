2bf6c209bf7bbbc803289da6ca3a05ac17d35087
developerthierry <developerthierry@gmail.com>
2025-11-21 01:41:54 +0000

    Load More Components Dynamically

--- App.jsx
+++ App.jsx
@@ -1,7 +1,27 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Entry from "./components/Entry";
+import data from "./data";
 
 export default function App() {
+  const entryElements = data.map((entry, index) => {
     return (
-        <Header />
-    )
+      <Entry
+      key={entry.id}
+        img={entry.img}
+        title={entry.title}
+        country={entry.country}
+        googleMapsLink={entry.googleMapsLink}
+        dates={entry.dates}
+        text={entry.text}
+      />
+    );
+  });
+  return (
+    <>
+      <Header />
+      <main className="container">
+       {entryElements}
+      </main>
+    </>
+  );
 }
